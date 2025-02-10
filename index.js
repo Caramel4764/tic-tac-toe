c9d8aa6336774f791bc77759ff67338176d4cdd8
Caramel4764 <nina.dong4764@gmail.com>
2025-02-09 19:55:56 -0800

    feat: game stop early if there is winner. Game display winner.

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ const board = document.getElementById("gameBoard");
 const gameGrid = document.getElementsByClassName("gameGrid");
 const restartBtn = document.getElementById('restartBtn');
 const gameOverMenu = document.getElementsByClassName('gameOverMenu')[0];
+const winnerTxt = document.getElementById("winnerTxt");
 const gameboard = (function(){
-  const board = [["", "", ""],["", "", ""],["", "", ""]];
-  let isGameOver = false;
+  let state = {
+    board: [["", "", ""],["", "", ""],["", "", ""]],
+    isGameOver: false,
+    winner: "Tie",
+  }
   const getBoard = ()=> {
-    return board;
+    return state.board;
   }
   const getIsGameOver = function () {
-    return isGameOver;
+    return state.isGameOver;
   }
   const getSquare = function(col, row) {
-    return board[row][col];
+    return state.board[row][col];
   }
   const hasEmptySpace = function () {
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
+    for (let i = 0; i < state.board.length; i++) {
+      for (let j = 0; j < state.board[i].length; j++) {
         //row/col
-        if (board[i][j] == "") {
+        if (state.board[i][j] == "") {
           return true;
         }
       }
@@ -26,25 +30,32 @@ const gameboard = (function(){
     return false;
   }
   const handleGameOver = function () {
-    //alert('game over');
-    isGameOver = true;
+    state.isGameOver = true;
     gameOverMenu.style.visibility = 'visible';
-    console.log({"gameover": isGameOver})
+    winnerTxt.textContent = state.winner;
+    state.winner="Tie";
   }
   const placeMove = function(col, row, val) {
-    board[row][col] = val;
-    if (!this.hasEmptySpace()) {
-      this.handleGameOver();
+    if (gameboard.getIsGameOver()==false) {
+      state.board[row][col] = val;
+      if (checkForWinner() != false) {
+        console.log(checkForWinner())
+        state.winner=checkForWinner();
+        this.handleGameOver();
+      }
+      else if (!this.hasEmptySpace()) {
+        this.handleGameOver();
+      }
     }
   }
   const clear = function() {
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
+    for (let i = 0; i < state.board.length; i++) {
+      for (let j = 0; j < state.board[i].length; j++) {
         //row/col
-        board[i][j] = "";
+        state.board[i][j] = "";
       }
     }
-    isGameOver = false;
+    state.isGameOver = false;
     console.log(getBoard())
     updateBoard();
     gameOverMenu.style.visibility='hidden';
@@ -54,7 +65,7 @@ const gameboard = (function(){
       if (getBoard()[Math.floor(i/3)][i%3] == player.playerInfo.marker) {
         gameGrid[i].classList.add('player');
         gameGrid[i].classList.remove('none');
-      } else if (getBoard()[Math.floor(i/3)][i%3] == game.AIInfo.marker) {
+      } else if (getBoard()[Math.floor(i/3)][i%3] == game.getAIInfo().marker) {
         gameGrid[i].classList.add('computer');
         gameGrid[i].classList.remove('none');
       } else if ((getBoard()[Math.floor(i/3)][i%3] == "")){
@@ -63,10 +74,74 @@ const gameboard = (function(){
         gameGrid[i].classList.add('none');
         gameGrid[i].classList.add('gameGrid');
       }
-      console.log(getBoard());
     }
   }
-  return {getBoard, getIsGameOver, placeMove, clear, getSquare, updateBoard, handleGameOver, hasEmptySpace}
+  function checkHori (marker) {
+    for (let i = 0; i<gameboard.getBoard().length; i++) {
+      let counter = 0;
+      for (let j = 0; j<gameboard.getBoard()[0].length; j++) {
+        if (gameboard.getSquare(j, i) == marker) {
+          counter++;
+          if (counter >= 3) {
+            console.log({horizontal: true})
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+  function checkVert (marker) {
+    for (let i = 0; i<gameboard.getBoard().length; i++) {
+      let counter = 0;
+      for (let j = 0; j<(gameboard.getBoard())[0].length; j++) {
+        if (gameboard.getSquare(i, j) == marker) {
+          counter++;
+          if (counter >= 3) {
+            console.log({vertical: true})
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+  function checkDiag(marker) {
+    let counter = 0;
+    for (let i = 0; i<gameboard.getBoard().length;i++) {
+      if (gameboard.getSquare(i, i) == marker) {
+        counter++;
+        console.log({marker: marker, count:counter})
+        if (counter>2) {
+          console.log({diag: true})
+          return true;
+        }
+      }
+    }
+    counter = 0;
+    for (let i = 0; i<gameboard.getBoard().length;i++) {
+      if (gameboard.getSquare(i, (gameboard.getBoard()[0].length-1)-i) == marker) {
+        counter++;
+        if (counter>2) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  //checks if anyone has won. False if no one and a player or pc if there's winner
+  function checkForWinner () {
+    if (checkHori(game.getAIInfo().marker) || checkVert(game.getAIInfo().marker) || checkDiag(game.getAIInfo().marker)) {
+      console.log({ai: true})
+      return "Computer";
+    } else if (checkVert(player.playerInfo.marker) || checkHori(player.playerInfo.marker) || checkDiag(player.playerInfo.marker)) {
+      console.log({player: true})
+      return "Player";
+    } else {
+      return false;
+    }
+  }
+  return {checkForWinner, getBoard, getIsGameOver, placeMove, clear, getSquare, updateBoard, handleGameOver, hasEmptySpace}
 })();
 
 const player = (function(){
@@ -83,76 +158,19 @@ const game = (function(){
     marker: "O",
   }
   function placeRandom() {
-    console.log(gameboard.getIsGameOver())
-    while(gameboard.getIsGameOver() == false) {
+    while (gameboard.hasEmptySpace()&&(gameboard.getIsGameOver()==false)) {
       let ranRow = Math.floor(Math.random()*3);
       let ranCol = Math.floor(Math.random()*3);
       if (gameboard.getSquare(ranCol, ranRow) == "") {
-        gameboard.placeMove(ranCol, ranRow, this.AIInfo.marker);
+        gameboard.placeMove(ranCol, ranRow, AIInfo.marker);
         return;
       }
     }
   }
-  function checkHori (marker) {
-    for (let i = 0; i<gameboard.getBoard().length; i++) {
-      let counter = 0;
-      for (let j = 0; j<gameboard.getBoard()[0].length; j++) {
-        if (gameboard.getSquare(j, i) == marker) {
-          counter++;
-          if (counter >= 3) {
-            return true;
-          }
-        }
-      }
-    }
-    return false;
+  function getAIInfo () {
+    return AIInfo;
   }
-  function checkVert (marker) {
-    for (let i = 0; i<gameboard.getBoard().length; i++) {
-      let counter = 0;
-      for (let j = 0; j<(gameboard.getBoard())[0].length; j++) {
-        if (gameboard.getSquare(i, j) == marker) {
-          counter++;
-          if (counter >= 3) {
-            return true;
-          }
-        }
-      }
-    }
-    return false;
-  }
-  function checkDiag(marker) {
-    let counter = 0;
-    for (let i = 0; i<gameboard.getBoard().length;i++) {
-      if (gameboard.getSquare(i, i) == marker) {
-        counter++;
-        if (counter>=2) {
-          return true;
-        }
-      }
-    }
-    counter = 0;
-    for (let i = 0; i<gameboard.getBoard().length;i++) {
-      if (gameboard.getSquare(i, (gameboard.getBoard()[0].length-1)-i) == marker) {
-        counter++;
-        if (counter>=2) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-  //checks if anyone has won. False if no one and a player or pc if there's winner
-  function checkForWinner () {
-    if (checkVert(AIInfo.marker) || checkHori(AIInfo.marker) || checkDiag(AIInfo.marker)) {
-      return "AI";
-    } else if (checkVert(player.playerInfo.marker) || checkHori(player.playerInfo.marker) || checkDiag(player.playerInfo.marker)) {
-      return "player";
-    } else {
-      return false;
-    }
-  }
-  return {placeRandom, checkForWinner, checkVert, AIInfo}
+  return {placeRandom, getAIInfo}
 }());
 restartBtn.addEventListener('click', function() {
   gameboard.clear();
@@ -162,7 +180,7 @@ for(let i = 0; i < gameGrid.length; i++) {
   gameGrid[i].addEventListener('click', function() {
     gameboard.placeMove(i%3, Math.floor(i/3), player.playerInfo.marker)
     game.placeRandom();
-    gameboard.updateBoard()
+    gameboard.updateBoard();
     //console.log(gameboard.getBoard());
   })
 }
